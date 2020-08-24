80f37c201e3de99e04814b38e765e860b5b53f4f
Surya Reddy <suryasreddy@gmail.com>
2020-08-24 16:17:21 -0700

    add hw07

--- homework/hw06/hw06.py
+++ homework/hw06/hw06.py
@@ -366,7 +366,7 @@ class Tree:
             return tree_str
         return print_tree(self).rstrip()
 def make_lambda (params, body):
-    params = params.replace("", '')
+    
     return lambda params: eval(body)
 
 
@@ -504,5 +504,78 @@ class Link:
     (if (null? (cdr s)) s
       (if (equal? (car s) (car (cdr s))) (no-repeats (cdr s))
         (cons (car s) (no-repeats (cdr s)))))) 
+
+(define (cddr s)
+  (cdr (cdr s)))
+
+(define (cadr s)
+  (car (cdr s))
+)
+
+(define (caddr s)
+  (car (cdr (cdr s)))
+)
+
+
+(define (sign num)
+  (cond
+    ((> num 0) 1)
+    ((= num 0) 0)
+    (else -1)
+)
+
+
+(define (square x) (* x x))
+
+(define (pow x y)
+  (cond
+    ((= n 0) 1)
+    ((even? n) (square (pow b (/ n 2))))
+    ((odd? n) (* b (square (pow b (/ (- n 1) 2)))))
+)
+
+
+(define (unique s)
+  (if (null? s) '()
+    (cons (car s) (unique (filter (lambda (x) (not (equal? x (car s))))
+      (cdr s)))))
+)
+
+
+(define (replicate x n)
+  (define (helper n acc)
+    (if (= n 0)
+      acc
+      (helper (- n 1) (cons x acc))))
+  (helper n nil))
+  )
+
+
+(define (accumulate combiner start n term)
+  (define (range n)
+    (define (helper x)
+      (if (> x n)
+        nil
+        (cons x (helper (+ 1 x)))))
+    (helper 1))
+    (define (reduce-default combiner lst default)
+    (reduce combiner (cons default lst)))
+  (reduce-default combiner (map term (range n)) start))
+)
+
+
+(define (accumulate-tail combiner start n term)
+  (define (helper x acc)
+    (if (> x n)
+      acc
+      (helper (+ 1 x) (combiner acc (term x)))))
+  (helper 1 start)
+)
+
+
+(define-macro (list-of map-expr for var in lst if filter-expr)
+  `(map (lambda (,var) ,map-expr) (filter (lambda (,var) ,filter-expr) ,lst))
+)
+
 
 
